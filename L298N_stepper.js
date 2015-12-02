bec3c34385baf99716e40a86a93d24184f0efdce
mogens jacobsen <artnode@users.noreply.github.com>
2015-12-02 10:11:32 +0100

    Create L298N_stepper.js

---
+++ L298N_stepper.js
@@ -0,0 +1,27 @@
+// MOJA 2015
+
+function L298step(mySteps){
+  var stepperPins = [C6,C7,C8,C9]; // Change these to your pins
+  var stepBits = [0b0110,0b0101,0b1001,0b1010];
+
+  if (mySteps<0) //reverse
+  {
+    stepBits.reverse();
+    mySteps = -mySteps;
+  }
+  
+  for (i=0; i< mySteps ; i++)
+  {
+    digitalWrite(stepperPins, stepBits[i % stepBits.length]);
+    //then we need to wait before sending next command
+    wait(3);
+  }
+
+}
+
+function wait(ms){
+  var d = new Date();
+  var d2 = null;
+  do { d2 = new Date(); }
+  while(d2-d < ms);
+}
